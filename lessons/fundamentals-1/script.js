2afe26bb26bf66bc596cc82aa20e61701623d72d
muhammedkazimcakmak <muhammed.cakmak@powercoders.org>
2021-05-27 11:53:36 +0200

    lesson2

---
+++ lessons/fundamentals-1/script.js
@@ -0,0 +1,46 @@
+/*
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+console.log("23");
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.14;
+console.log(PI);
+
+let myFirstJob = "Security Analyst";
+let myCurrentJob = "coder";
+console.log(myFirstJob);
+console.log(myCurrentJob);
+*/
+
+//
+true;
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof javascriptIsFun);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1987;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
